ce24a94ebe466ce0f927c3f67c82042efbed732f
kksingh9 <kksingh0921@gmail.com>
2023-06-30 01:17:33 +0530

    addtocart

--- Cart/AddToCart.jsx
+++ Cart/AddToCart.jsx
@@ -1,83 +1,95 @@
-import React from "react";
-import { Modal, Button, Row } from "react-bootstrap";
+import React, { useContext, useCallback, useEffect} from "react";
+import { Modal, Button } from "react-bootstrap";
 import CartItem from "./CartItem";
+import CartContext from "../../Store/CartContext";
+import { useState } from "react";
 
 const CartElements = (props) => {
-    const cartElements = [
+  const cartCtx = useContext(CartContext);
+    const itemData = cartCtx.items
+  const [loadingCartData, setLoadingCartData ] = useState([]);
+  const EmailId = cartCtx.emailId;
+  console.log(EmailId);
+  
+  const abcGet = useCallback(() => {
+    
+    fetch(`https://crudcrud.com/api/2ca6a3cd42454b4aaaae3887d5b11c04/cart${EmailId}`)
+    .then((response) => {
+      if(response.ok){
+    return response.json()
+      }else{
+        throw new Error("something went wrong!");
+      }
+      
+    }).then((data) =>{
+      console.log(data);
+    let cartData = [];
+      for(let key in data){
+        cartData.push({
+          
+          id: data[key]._id ,
+          title: data[key].title,
+          imageUrl: data[key].imageUrl,
+          quantity: data[key].quantity,
+          price: data[key].price
+        })
+      }
+      cartCtx.addItem(data)
+      setLoadingCartData(cartData);
+    }).catch((err) => {
+        console.log(err.message);
+    });
+      
+  },[EmailId]);
+  
+  useEffect(() => {
+  abcGet();
+  
+  },[abcGet]);
 
-        {
-        
-        title: 'Colors',
-        
-        price: 100,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-        
-        quantity: 2,
-        
-        },
-        
-        {
-        
-        title: 'Black and white Colors',
-        
-        price: 50,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-        
-        quantity: 3,
-        
-        },
-        
-        {
-        
-        title: 'Yellow and Black Colors',
-        
-        price: 70,
-        
-        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-        
-        quantity: 1,
-        
-        }
-        
-        ]
-        
-        
-    return(
-            <AddToCart cartElements={cartElements} onHideCart={props.onHideCart} showCart={props.showCart} />
-    )
-}
+  // console.log(loadingCartData);
 
+  return (
+    <AddToCart
+      cartData={loadingCartData}
+      onHideCart={props.onHideCart}
+      showCart={props.showCart}
+    />
+  );
+};
 
 const AddToCart = (props) => {
-    return (
-        <div 
-        className="modal show"
-        style={{ position: 'initial'}}
-        >
-        <Modal show={props.showCart} onHide={props.onHideCart}>
+  const ctx = useContext(CartContext);
+  const TotalAmount = `${ctx.totalAmount}`;
+  return (
+    <div className="modal show" style={{ position: "initial" }}>
+      <Modal show={props.showCart} onHide={props.onHideCart}>
         <Modal.Header closeButton>
-            <Modal.Title>
-            </Modal.Title>
+          <Modal.Title></Modal.Title>
         </Modal.Header>
-            
-        {props.cartElements.map((item) =>  (
-        <CartItem title={item.title} price={item.price} imageUrl={item.imageUrl} quantity={item.quantity} />
 
+        {props.cartData.map((item) => (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            quantity={item.quantity}
+          />
         ))}
-    
-      
+
         <Modal.Body>
-            <h1>Total<span>Rs 0</span></h1>
+          <h1>
+            Total amount {" "}<span>Rs {TotalAmount}</span>
+          </h1>
         </Modal.Body>
         <Modal.Footer>
-            <Button variant="primary">Purchase</Button>
+          <Button variant="primary">Purchase</Button>
         </Modal.Footer>
-        </Modal>
-        </div>
-
-    );
+      </Modal>
+    </div>
+  );
 };
 
 export default CartElements;
